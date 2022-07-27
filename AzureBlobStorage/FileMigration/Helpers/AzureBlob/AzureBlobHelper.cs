ba562a6f7dda3905774d1d79c20373dc6b515ac3
FERNANDO NOVACKI FREITAS <fernando.freitas3@fatec.sp.gov.br>
2022-07-26 22:37:52 -0300

    initialize project

---
+++ AzureBlobStorage/FileMigration/Helpers/AzureBlob/AzureBlobHelper.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Storage;
+using Azure.Storage.Blobs;
+
+namespace FileMigration.Helpers.AzureBlob
+{
+    public static class AzureBlobHelper
+    {
+        public const string ACCOUNT_NAME = "";
+        public const string ACCOUNT_KEY = "";
+        public const string CONNECTION_STRING = "";
+        public static BlobServiceClient GenerateBlobServiceClient() =>
+            new(new Uri(GetBlobUri()), GenerateStorageSharedKeyCredential());
+        public static BlobServiceClient GenerateBlobServiceClientByConnectionString() =>
+             new(CONNECTION_STRING);
+        public static StorageSharedKeyCredential GenerateStorageSharedKeyCredential() =>
+            new (ACCOUNT_NAME, ACCOUNT_KEY);
+        public static string GetBlobUri() =>
+            $"https://{ACCOUNT_NAME}.blob.core.windows.net";
+    }
+}
